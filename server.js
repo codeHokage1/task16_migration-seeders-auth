0ebe252d04080388cb7f7e382cd28a8320f51151
Sodiq Farhan <sodiqfarhan360@gmail.com>
2023-09-21 19:41:32 +0100

    Setup general files and dirs

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const categoriesRoutes = require("./routes/categoriesRoutes");
+const itemsRoutes = require("./routes/itemsRoutes");
+const ordersRoutes = require("./routes/ordersRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("Welcome to my API!");
+});
+
+app.use("/users", userRoutes);
+app.use("/categories", categoriesRoutes);
+app.use("/items", itemsRoutes);
+app.use("/orders", ordersRoutes);
+
+app.get("*", (req, res) => {
+	res.status(404).json({
+		message: "Route not found",
+		data: null
+	});
+});
+
+const PORT = process.env.PORT || 3000; 
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
