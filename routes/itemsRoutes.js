aa255ad4616c59163fee81937380fa3aa1495a59
Sodiq Farhan <sodiqfarhan360@gmail.com>
2023-09-26 08:50:41 +0100

    Update items routes to validate incoming crud requests

--- routes/itemsRoutes.js
+++ routes/itemsRoutes.js
@@ -2,10 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const itemControllers = require("../controllers/itemsControllers");
+const validRequest = require("../middlewares/validRequest");
 const auth = require("../middlewares/auth");
 
 router.use(auth.validateAccess);
 
-router.get("/", itemControllers.getAllItems);
+router
+    .get("/", itemControllers.getAllItems)
+    .post("/", itemControllers.createItem)
+    .put("/:id", itemControllers.updateItem)
+    .delete("/:id", itemControllers.deleteItem);
 
 module.exports = router;
